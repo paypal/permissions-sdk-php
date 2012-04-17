46c6aa1f92cb4d396d70b32b2bc294509b377993
tarini <tarininegi@gmail.com>
2012-04-17 17:39:51 +0530

    stubs update for pp wsdl version 88.0

--- lib/PPUtils.php
+++ lib/PPUtils.php
@@ -1,19 +1,19 @@
 <?php
 class PPUtils {
-	
+
 	const SDK_VERSION = "0.6";
-	const SDK_NAME = "PHP Invoice SDK ";
-	
+	const SDK_NAME = "sdk-permission-php";
+
 	/**
-	 * 
-	 * Convert a Name Value Pair (NVP) formatted string into 
+	 *
+	 * Convert a Name Value Pair (NVP) formatted string into
 	 * an associative array taking care to urldecode array values
-	 * 
+	 *
 	 * @param string $nvpString
 	 */
 	public static function nvpToMap($nvpString) {
-		
-		$ret = array();		
+
+		$ret = array();
 		$params = explode("&", $nvpString);
 		foreach($params as $p) {
 			list($k, $v) = explode("=", $p);
@@ -21,7 +21,7 @@ class PPUtils {
 		}
 		return $ret;
 	}
-	
+
 	/**
 	 * Returns true if the array contains a key like $key
 	 * @param array $map
@@ -32,21 +32,21 @@ class PPUtils {
 		$key = str_replace(")", "\)", $key);
 		$key = str_replace(".", "\.", $key);
 		$pattern = "/$key*/";
-		foreach($map as $k => $v) {		
+		foreach($map as $k => $v) {
 			preg_match($pattern, $k, $matches);
 			if(count($matches) > 0)
 				return true;
 		}
 		return false;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Get the local IP address. The client address is a required
 	 * request parameter for some API calls
 	 */
 	public static function getLocalIPAddress() {
-		
+
 		if(array_key_exists("SERVER_ADDR", $_SERVER)) {
 			// SERVER_ADDR is available only if we are running the CGI SAPI
 			return $_SERVER['SERVER_ADDR'];
@@ -60,7 +60,7 @@ class PPUtils {
 	}
 
 	/**
-	 * 
+	 *
 	 * Compute the value that needs to sent for the PAYPAL_REQUEST_SOURCE
 	 * parameter when making API calls
 	 */
