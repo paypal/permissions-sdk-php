84b7781ee60c2cc9fb993d3e24d0a9bda6a56b47
Ganesh Hegde <1ganesh1@facebook.com>
2013-03-21 16:33:52 +0530

    adding support for dynamic configuration

--- lib/services/Permissions/PermissionsService.php
+++ lib/services/Permissions/PermissionsService.php
@@ -17,8 +17,8 @@ class PermissionsService extends PPBaseService {
 	// SDK Version
 	protected static $SDK_VERSION = "2.2.98";
 
-	public function __construct() {
-		parent::__construct(self::$SERVICE_NAME, 'NV', array('PPPlatformServiceHandler'));
+	public function __construct($config = null) {
+		parent::__construct(self::$SERVICE_NAME, 'NV', array('PPPlatformServiceHandler'), $config);
         parent::$SDK_NAME    = self::$SDK_NAME ;
         parent::$SDK_VERSION = self::$SDK_VERSION;
 	}
