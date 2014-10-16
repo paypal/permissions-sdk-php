2a84fe50dadbb89289161ab10f1fb57d7684e6fc
japatel <japatel@paypal.com>
2014-10-16 11:52:53 -0500

    - Updated samples to not use its own composer.json

--- samples/PPBootStrap.php
+++ samples/PPBootStrap.php
@@ -6,18 +6,18 @@
 
 //
 require_once 'Configuration.php';
-/*
- * @constant PP_CONFIG_PATH required if credentoal and configuration is to be used from a file
-* Let the SDK know where the sdk_config.ini file resides.
-*/
-//define('PP_CONFIG_PATH', dirname(__FILE__));
 
-/*
- * use autoloader
-*/
-if(file_exists( dirname(__FILE__). '/vendor/autoload.php')) {
-require 'vendor/autoload.php';
-} else {
-require 'PPAutoloader.php';
-    PPAutoloader::register();
+// Include the composer autoloader
+// The location of your project's vendor autoloader.
+$composerAutoload = dirname(dirname(dirname(__DIR__))) . '/autoload.php';
+if (!file_exists($composerAutoload)) {
+    //If the project is used as its own project, it would use rest-api-sdk-php composer autoloader.
+    $composerAutoload = dirname(__DIR__) . '/vendor/autoload.php';
+
+
+    if (!file_exists($composerAutoload)) {
+        echo "The 'vendor' folder is missing. You must run 'composer update' to resolve application dependencies.\nPlease see the README for more information.\n";
+        exit(1);
+    }
 }
+require $composerAutoload;
